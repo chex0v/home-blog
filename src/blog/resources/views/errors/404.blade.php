ffdfe621bb2c053226fd4665dbd35c0efd85904e
chexov <chex0ff@inbox.ru>
2022-05-02 23:59:25 +0300

    feature Added routes param throw nginx and php-fpm

---
+++ src/blog/resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('header-css')
+    @parent
+@endsection
+@section('title', 'Страница не найдена')
+
+@section('content')
+    Страница не найдена
+@endsection
---
+++ src/blog/resources/views/pages/blog/list.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('header-css')
+    @parent
+@endsection
+@section('title', 'Блог')
+
+@section('content')
+    Список страниц блога
+@endsection
