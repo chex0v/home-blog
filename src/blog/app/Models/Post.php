6ddb8afc6fd62c2730b8c96894a2f99068b5ed84
chexov <chex0ff@inbox.ru>
2022-05-03 15:29:52 +0300

    feature Added Post model and Factory and Seeder

---
+++ src/blog/app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @class Post
+ * @property int $id Идентификатор
+ * @property string $title Название
+ * @property string $text Описание
+ */
+class Post extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'blog';
+}
